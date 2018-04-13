d9299e79b22e94d0e794120b196e91eb75cae249
Manfred9 <manfredgh@hotmailo.com>
2018-04-12 19:21:45 -0600

    Project commit

---
+++ Interface/ProyectoDos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+/**
+ *
+ * @author manfr
+ */
+public class ProyectoDos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaPrimera vp = new VentanaPrimera();
+        vp.setVisible(true);
+        vp.setLocationRelativeTo(null);
+    }
+    
+}
